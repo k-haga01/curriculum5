19fee4113cc1e89c1c319fffc640f3b1a50e7ecf
k-haga01 <keitahaga.118@gmail.com>
2022-02-14 20:47:31 +0900

    make auth実装

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
